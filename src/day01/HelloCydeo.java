4b4dff21cf1ef896f28e924bedb21d864223f919
Mike <112992682+MihanikMike@users.noreply.github.com>
2025-03-16 09:22:27 -0400

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -4,5 +4,6 @@ public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("HelloCydeo");
         System.out.println("haha");
+        // Some changes are added.
     }
 }
